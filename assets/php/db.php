079c92a993d17c9468add4c04326f934fb905449
MikeF. <102179118+mkrosz@users.noreply.github.com>
2024-09-29 23:14:06 -0300

    Update db.php
    Removi as aspas tudo.

--- assets/php/db.php
+++ assets/php/db.php
@@ -11,7 +11,7 @@ $DB_NAME =$_ENV['DB_NAME']; // o nome do banco de dados
 $DB_PORT =$_ENV['DB_PORT'];
 
 // Create connection
-$conn = mysqli_connect('$DB_HOST', '$DB_USER', '$DB_PASSWORD', '$DB_NAME', '$DB_PORT');
+$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 
 // Check connection
 if ($conn->connect_error) {
